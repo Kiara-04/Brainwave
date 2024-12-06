43cba4b246b47116bc59313074b5e3e8201cf2fb
Kiara <kj.addinall@gmail.com>
2024-12-06 13:06:26 +0200

    fix attempt 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Header from "./components/Header";
-import Hero from "./components/hero";
+import Hero from "./components/Hero"; // :)
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
